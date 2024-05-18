22de76dcea654a4bbb3465a0b5a9dca1dd334718
alsunj <alsunj@ttu.ee>
2024-05-18 15:47:36 +0300

    change trafficReports to WebApp

--- Base.Contracts.Domain/obj/Debug/net8.0/Base.Contracts.Domain.AssemblyInfo.cs
+++ Base.Contracts.Domain/obj/Debug/net8.0/Base.Contracts.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Base.Contracts.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10deab813b2aa32213e8dec00506de4404dd994")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07db420cb3a695f4851fbfa73135e9f97615a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Base.Contracts.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Base.Contracts.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
